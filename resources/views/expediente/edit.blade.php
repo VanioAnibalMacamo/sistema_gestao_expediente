b8a14cbf3044d1a8ea3f3f0c76c1491cb85b55c4
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2023-08-21 09:24:16 +0200

    Melhorar o Bug em que ao visualizar o expediente o campo do Estudante não aparece pre-seleccionado.

--- resources/views/expediente/edit.blade.php
+++ resources/views/expediente/edit.blade.php
@@ -68,20 +68,22 @@
                                             <div class="col-md-6">
                                                 <div class="form-group">
                                                     <label for="estudante_id">Estudante</label>
-                                                    <select class="form-control" id="estudante_id" name="estudante_id"
-                                                        @if (auth()->user()->tipo_usuario === 'Estudante') disabled @endif>
+                                                    <select class="form-control" id="estudante_id" name="estudante_id">
                                                         @if (auth()->user()->tipo_usuario === 'Estudante')
                                                             <option value="{{ auth()->user()->userable->id }}" selected>
                                                                 {{ auth()->user()->userable->codigo }} - {{ auth()->user()->userable->nome }} {{ auth()->user()->userable->apelido }}
                                                             </option>
                                                         @else
-                                                            <option value="">Selecione o Estudante</option> <!-- Adicionada opção "Select" -->
+                                                            <option value="">Selecione o Estudante</option>
                                                             @foreach ($estudantes as $estudante)
-                                                                <option value="{{ $estudante->id }}">{{ $estudante->codigo." - ".$estudante->nome." ".$estudante->apelido }}</option>
+                                                                <option value="{{ $estudante->id }}" @if ($estudante->id == $expediente->estudante->id) selected @endif>
+                                                                    {{ $estudante->codigo." - ".$estudante->nome." ".$estudante->apelido }}
+                                                                </option>
                                                             @endforeach
                                                         @endif
                                                     </select>
                                                 </div>
+                                            </div>
                                             </div>
                                         </div>
                                         <div class="row">
